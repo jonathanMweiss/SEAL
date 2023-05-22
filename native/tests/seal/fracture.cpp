2134e24c5cc8e5ff1f8ab11dce9cb3604864779e
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2023-05-22 08:10:44 +0300

    refactor

--- native/tests/seal/fracture.cpp
+++ native/tests/seal/fracture.cpp
@@ -13,30 +13,43 @@
 #include <cstdint>
 #include <ctime>
 #include <string>
+#include <utility>
 #include "gtest/gtest.h"
 
 using namespace seal;
 using namespace std;
 namespace sealtest
 {
+    struct SetupObjs
+    {
+        seal::EncryptionParameters enc_params;
+        seal::SEALContext context;
+        seal::fractures::Essence essence;
+        seal::Evaluator evaluator;
+        static SetupObjs New(std::uint64_t N = 4096, int logt = 20)
+        {
+            seal::EncryptionParameters enc(seal::scheme_type::bgv);
+
+            enc.set_poly_modulus_degree(N);
+            enc.set_coeff_modulus(seal::CoeffModulus::BFVDefault(N));
+            enc.set_plain_modulus(seal::PlainModulus::Batching(N, logt + 1));
+
+            return SetupObjs(enc);
+        }
+
+        explicit SetupObjs(seal::EncryptionParameters encryption_params)
+            : enc_params(std::move(encryption_params)), context(enc_params, true), essence(context, enc_params),
+              evaluator(context)
+        {}
+    };
 
     TEST(FracturedOps, Poly)
     {
-        // enc params
-        std::uint64_t N = 4096;
-        int logt = 20;
-        seal::EncryptionParameters enc_params(seal::scheme_type::bgv);
-        enc_params.set_poly_modulus_degree(N);
-        enc_params.set_coeff_modulus(seal::CoeffModulus::BFVDefault(N));
-        enc_params.set_plain_modulus(seal::PlainModulus::Batching(N, logt + 1));
+        auto all = SetupObjs::New();
 
-        // context
-
-        seal::Plaintext p("1x^2 + 2x^1 + 3x^0");
-        seal::SEALContext context_data(enc_params, true);
-        seal::fractures::Essence e(context_data, enc_params);
-
-        seal::fractures::Poly p_frac(p, 3, 3, 2);
+        // test
+        seal::Plaintext ptx("1x^2 + 2x^1 + 3x^0");
+        seal::fractures::Poly::crate_plaintext(all.evaluator, all.essence, 2, ptx);
 
         std::cout << "hello friend" << std::endl;
     }
