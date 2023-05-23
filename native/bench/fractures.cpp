b60ced6b3fc21534fcb1309d60c077c9be7663bf
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2023-05-23 08:07:19 +0300

    benchmarking plain_mult

--- native/bench/fractures.cpp
+++ native/bench/fractures.cpp
@@ -12,15 +12,7 @@ namespace sealbench
 
     void polynomial_fracture(State &state, shared_ptr<BMEnv> bm_env)
     {
-        //        auto all = SetupObjs::New();
         std::uint64_t num_fractures = 256;
-
-        //        seal::Ciphertext encrypted_ntt = all.random_ciphertext();
-        //        auto ptx = all.random_plaintext();
-        //        all.evaluator.transform_to_ntt_inplace(ptx, encrypted_ntt.parms_id());
-
-        //        seal::Ciphertext encrypted_ntt_cpy(encrypted_ntt);
-        //        seal::Plaintext ptx_cpy(ptx);
         seal::fractures::Essence essence(bm_env->context(), bm_env->parms());
         seal::Plaintext ptx;
         bm_env->randomize_pt_bgv(ptx);
@@ -28,6 +20,42 @@ namespace sealbench
         for (auto _ : state)
         {
             seal::fractures::Polynomial pshredder(ptx, essence, num_fractures);
+        }
+    }
+
+    void fracture_ctx(State &state, shared_ptr<BMEnv> bm_env)
+    {
+        std::uint64_t num_fractures = 256;
+        seal::fractures::Essence essence(bm_env->context(), bm_env->parms());
+
+        seal::Ciphertext ctx;
+        bm_env->randomize_ct_bgv(ctx);
+        for (auto _ : state)
+        {
+            seal::fractures::CiphertextShredder cshred(ctx, essence, num_fractures);
+        }
+    }
+
+    void fractured_plain_mult(State &state, shared_ptr<BMEnv> bm_env)
+    {
+        std::uint64_t num_fractures = 256;
+        seal::fractures::Essence essence(bm_env->context(), bm_env->parms());
+
+        seal::Ciphertext ctx;
+        bm_env->randomize_ct_bgv(ctx);
+
+        seal::Plaintext ptx;
+        bm_env->randomize_pt_bgv(ptx);
+
+        seal::fractures::Polynomial pshredder(ptx, essence, num_fractures);
+        seal::fractures::CiphertextShredder cshredder(ctx, essence, num_fractures);
+
+        for (auto _ : state)
+        {
+            for (std::uint64_t i = 0; i < num_fractures; ++i)
+            {
+                cshredder[i] *= pshredder.get_fracture(i);
+            }
         }
     }
 } // namespace sealbench
